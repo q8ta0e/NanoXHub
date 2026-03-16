dc467acf3c0fb65032bc79e55aeea4057701a163
NPC-Q8 <q8ta0e@gmail.com>
2026-03-16 23:20:18 +0700

    Create SwitchHubKaitun.lua

---
+++ SwitchHubKaitun.lua
@@ -0,0 +1,4 @@
+local v0 = require(game:GetService("ReplicatedStorage").Notification);
+v0.new("<Color=Yellow>NPC-Q8: Switch Hub Kaitun chỉ là Fake thôi<Color=/>"):Display();
+wait(0.5);
+v0.new("<Color=Yellow>NPC-Q8: Đã đóng cửa vì quá lỗi thời và rất nhiều lỗi không fix được<Color=/>"):Display();
